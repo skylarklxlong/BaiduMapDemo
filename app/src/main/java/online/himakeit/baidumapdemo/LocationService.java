b054c92110cc3f143a76decc20d5b977220922ee
Skylark <li_xuelong@126.com>
2018-02-28 18:46:19 +0800

    fix bug

--- app/src/main/java/online/himakeit/baidumapdemo/LocationService.java
+++ app/src/main/java/online/himakeit/baidumapdemo/LocationService.java
@@ -82,17 +82,29 @@ public class LocationService {
         if (mOption == null) {
             mOption = new LocationClientOption();
             /**
-             * 可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+             * 可选，默认高精度，设置定位模式
+             * 高精度 LocationMode.Hight_Accuracy
+             * 低功耗 LocationMode. Battery_Saving
+             * 仅使用设备 LocationMode. Device_Sensors
              */
             mOption.setLocationMode(LocationMode.Hight_Accuracy);
             /**
              * 可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
+             * gcj02：国测局坐标；
+             * bd09ll：百度经纬度坐标；
+             * bd09：百度墨卡托坐标；
+             * 海外地区定位，无需设置坐标类型，统一返回wgs84类型坐标
              */
             mOption.setCoorType("bd09ll");
             /**
              * 可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
              */
             mOption.setScanSpan(3000);
+            /**
+             * 可选，设置是否使用gps，默认false
+             * 使用高精度和仅用设备两种定位模式的，参数必须设置为true
+             */
+            mOption.setOpenGps(true);
             /**
              * 可选，设置是否需要地址信息，默认不需要
              */
@@ -129,7 +141,15 @@ public class LocationService {
              * 可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
              */
             mOption.setIsNeedAltitude(false);
-
+            /**
+             * 可选，7.2版本新增能力
+             * 如果设置了该接口，首次启动定位时，会先判断当前WiFi是否超出有效期，若超出有效期，会先重新扫描WiFi，然后定位
+             */
+            mOption.setWifiCacheTimeOut(5*60*1000);
+            /**
+             * 可选，设置是否需要过滤GPS仿真结果，默认需要，即参数为false
+             */
+            mOption.setEnableSimulateGps(false);
         }
         return mOption;
     }
